c7b6645f33dd5063eaa345c95ebccc7512c33a07
Denis Shakurov <den_shak@mail.ru>
2024-01-15 16:14:29 +0300

    Add Homework 2

---
+++ HomeWork2/main.py
@@ -0,0 +1,9 @@
+def print_multiplication_table(n):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            print(f'{i} * {j} = {i * j}')
+        print()
+
+n = int(input("Введите число n: "))
+print_multiplication_table(n)
+
